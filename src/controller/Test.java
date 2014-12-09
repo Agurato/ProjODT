769cfe77c3f37a782e7099241ad7751e358d0093
Agurato <vincent1611@hotmail.fr>
2014-12-09 18:26:53 +0100

    Just to test the methods in "ODTFile.java"

--- src/controller/Test.java
+++ src/controller/Test.java
@@ -1,5 +1,20 @@
 package controller;
 
-public class Test {
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import model.*;
+//import view.*;
+
+public class Test {
+	public static void main(String []args) {
+		ODTFile file = new ODTFile("/home/vincent/Documents/odt/exemple_fichier_ttx_odt.odt");
+		try {
+			file.parseContentXML(file.unzipODT(), "Contraintes");
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
