868e9eb11fb9c0e4b08367908ce1c61c11a54bf9
Agurato <vincent1611@hotmail.fr>
2014-12-09 18:24:33 +0100

    the parseContentXML now finds what we send as parameters

--- src/model/ODTFile.java
+++ src/model/ODTFile.java
@@ -10,6 +10,21 @@ import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * 
+ * @author vincent
+ *
+ */
+
 public class ODTFile implements TextFile {
 	private File odt = null;
 	private File repository = null;
@@ -25,7 +40,7 @@ public class ODTFile implements TextFile {
 		return path;
 	}
 
-	public void unzipODT() throws FileNotFoundException, IOException {
+	public File unzipODT() throws FileNotFoundException, IOException {
 		// Create the folder where it will be unzipped
 		File folder = new File(repository.getAbsolutePath()+"/"+odt.getName().replace(".odt", ""));
 		folder.mkdir();
@@ -64,6 +79,107 @@ public class ODTFile implements TextFile {
 		finally {
 			zipI.close();
 		}
+		return folder;
+	}
+	
+	public void parseContentXML(File folder, CharSequence search) {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document parser = builder.parse(new File(folder.getAbsolutePath(), "content.xml"));
+			
+			Element root = parser.getDocumentElement();
+			// This is the "office:document-content", which contains everything in the content.xml file
+			NodeList rootNodes = root.getChildNodes();
+			// We get his children ("office:scripts", "office:font-face-decls", "office:automatic-styles" & "office:body")
+			
+			for(int i=0 ; i<rootNodes.getLength() ; i++) {
+				// Parse root children and get the "office:body" one
+				if(rootNodes.item(i).getNodeName().equals("office:body")) {
+					
+					Element officeBody = (Element) rootNodes.item(i);
+					NodeList officeBodyNodes = officeBody.getChildNodes();
+					
+					for(int j=0 ; j<officeBodyNodes.getLength() ; j++) {
+						// Parse "office:body" children and get the "office:text" one, even if it should be the only child
+						if(officeBodyNodes.item(j).getNodeName().equals("office:text")) {
+						
+							Element officeText = (Element) officeBodyNodes.item(j);
+							NodeList textHList = officeText.getElementsByTagName("text:h");
+							NodeList textPList = officeText.getElementsByTagName("text:p");
+							
+							for(int k=0 ; k<textHList.getLength() ; k++) {
+								Element textH = (Element) textHList.item(k);
+								System.out.println(textH.getTextContent());
+								
+								if(textH.getTextContent().contains(search.toString())) {
+									System.out.println("** found **");
+								}
+							}
+							
+							for(int k=0 ; k<textPList.getLength() ; k++) {
+								Element textP = (Element) textPList.item(k);
+								if(!(textP.getAttribute("text:style-name").startsWith("Text") ||
+									 textP.getAttribute("text:style-name").startsWith("Content"))) {
+									System.out.println(textP.getTextContent());
+									
+									if(textP.getTextContent().contains(search.toString())) {
+										System.out.println("** found **");
+									}
+								}
+							}
+							
+//							for(int k=0 ; k<officeTextNodes.getLength() ; k++) {
+//								System.out.println(officeTextNodes.item(k).getNodeName());
+//								
+//								if(officeTextNodes.item(k).getNodeName().equals("text:h")) {
+//									System.out.println("\tSearching in \"text:h\" ...");
+//									
+//									
+//								}
+//								
+//								
+//								
+//								
+//								
+//								
+//								
+////								// Parse "office:text" children
+////								Element element = (Element) officeTextNodes.item(k);
+////								// Takes the k-th line
+////								Element attribute = (Element) element.getElementsByTagName("text:syle-name").item(0);
+////								// Takes the attribute of the line
+////								try {
+////									content = attribute.getTextContent();
+////									// Get the content of the attribute
+////								}
+////								catch(NullPointerException npe) {
+////									npe.printStackTrace();
+////								}
+////								
+////								// If (the node is a HEADING type) OR ((it is a PARAGRAPH type) AND (it isn't a TEXT type))
+////								if(element.getNodeName().equals("text:h") ||
+////									(element.getNodeName().equals("text:p") && !content.startsWith("Text"))) {
+////									if(content.contains(search.toString())) {
+////										System.out.println("found");
+////									}
+////								}
+//							}
+						}
+					}
+				}
+			}
+		}
+		catch(ParserConfigurationException pce) {
+			pce.printStackTrace();
+		}
+		catch(SAXException saxe) {
+			saxe.printStackTrace();
+		}
+		catch(IOException ioe) {
+			ioe.printStackTrace();
+		}		
 	}
 	/*
 		for(File file : folder.listFiles()) {
