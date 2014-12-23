df41098f939a1254dd8353792b14ad029a93e81f
Louis Desportes <louis@akkes.fr>
2014-12-23 15:46:09 +0100

    GNUification of command line + help

--- src/controller/Console.java
+++ src/controller/Console.java
@@ -10,17 +10,20 @@ public class Console {
 		for(int i=0 ; i<args.length-1 ; i++) {
 			switch(args[i]) {
 			case "-d" :
+			case "--database" :
 				db = new DataBase(args[i+1]);
 				i++;
 				
 				break;
 			case "-f" :
+			case "--file" :
 				db = new DataBase();
 				db.addOdt(args[i+1]);
 				i++;
 				
 				break;
 			case "-w" :
+			case "--word" :
 				if(db != null) {
 					db.search(args[i+1]);
 				}
@@ -31,8 +34,13 @@ public class Console {
 				
 				break;
 			default :
-				
-				break;
+				System.out.println("ProjODT - v1.0 - (c) Vincent Monot & Louis Desportes");
+				System.out.println("Utilitaire de recherche dans une base d'ODT");
+				System.out.println("");
+				System.out.println("Commandes:");
+				System.out.println("  -d,  --database : défini la racine de la base de donnés à utiliser");
+				System.out.println("  -f,  --file: ajoute un fichier à la base de donnés");
+				System.out.println("  -w,  --word : recherche le mot dans la base de donnés");
 			}
 		}
 	}
